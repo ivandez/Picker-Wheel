3173c778538cb85050afbc6a8c71d982bc5b3768
ivandez <ivan.mendez.dev@gmail.com>
2024-04-01 15:33:01 -0400

    delete library and imported wheele component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import FortuneWheel from "./components/FortuneWheel";
 
 function App() {
+  const segments = ["better luck next time", "won 70", "won 10"];
+
+  const segColors = ["#EE4040", "#F0CF50", "#815CD1"];
+
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
 
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
-  )
+    <FortuneWheel
+      segments={segments}
+      segColors={segColors}
+      onFinished={(winner) => onFinished(winner)}
+      primaryColor="black"
+      contrastColor="white"
+      buttonText="Spin"
+      isOnlyOnce={false}
+      size={290}
+      upDuration={250}
+      downDuration={600}
+      fontFamily="Arial"
+    />
+  );
 }
 
-export default App
+export default App;
