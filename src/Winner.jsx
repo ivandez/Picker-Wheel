dd8d321252a3a2ce94338c9f19782300474eb568
ivandez <ivan.mendez.dev@gmail.com>
2024-04-03 20:15:22 -0400

    wip winner card

--- src/Winner.jsx
+++ src/Winner.jsx
@@ -1,13 +1,21 @@
 import useStore from "./hooks/useStore";
 
+import "./winner.css";
+
 const Winner = () => {
   const winner = useStore((state) => state.winner);
 
   return (
-    <>
-      <div>Winner</div>
-      <h1>{winner.name}</h1>
-    </>
+    <section className="winner-container">
+      <div className="winner-wrap">
+        <div className="border"></div>
+        <h1>Congradulations!</h1>
+        <h2>The winner is:</h2>
+        <div className="winner-ribbon">{winner.name}</div>
+        <div className="right-ribbon"></div>
+        <div className="left-ribbon"></div>
+      </div>
+    </section>
   );
 };
 
